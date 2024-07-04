84ca27d45291e8a73dab8dd8bfd82ef9ef7e9585
Matheus Milani <32438065+mmilani1@users.noreply.github.com>
2024-07-04 15:06:41 -0300

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.default_prog = { wezterm.home_dir .. '/.local/bin/zellij', '-l', 'welcome' }
 config.color_scheme = 'GruvboxDark'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 
